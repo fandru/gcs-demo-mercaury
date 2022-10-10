305eb9da28460d3984984d3f14d1f85bd1aff28e
fnelsonanburaj <39556131+fandru@users.noreply.github.com>
2022-10-10 15:49:30 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,35 @@
+resource "google_storage_bucket" "bucket_07" {
+
+name = "my-terraform-bucket-for-mercaury07" 
+
+storage_class = "REGIONAL"
+
+project = "mytasks-362509"
+
+location = "us-central1"
+  
+
+}
+
+resource "google_storage_bucket" "bucket_08" {
+
+    name = "my-terraform-bucket-for-mercaury08"
+
+    storage_class = "REGIONAL"
+
+    project = "mytasks-362509"
+
+    location = "us-central1"
+
+  
+}
+
+
+
+
+
+
+
+
+
+
