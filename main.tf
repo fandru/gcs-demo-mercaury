1cd2d0aaab43dda437a6cf1e79a12224cb17e442
fnelsonanburaj <39556131+fandru@users.noreply.github.com>
2022-10-11 19:29:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 
-resource "google_storage_bucket" "frontend_storage01" {
+resource "google_storage_bucket" "frontend_storage001" {
 
-name = "mercaury_frontend_data01" 
+name = "mercaury_frontend_data001" 
 
 storage_class = "REGIONAL"
 
@@ -12,21 +12,9 @@ location = "us-central1"
 
 }
 
-resource "google_storage_bucket" "backend_storage02" {
+resource "google_storage_bucket" "backend_storage002" {
 
-    name = "mercaury_backend_data02"
-
-    storage_class = "REGIONAL"
-
-    project = "mytasks-362509"
-
-    location = "us-central1"
-
-  
-}
-resource "google_storage_bucket" "data_engine_storage03" {
-
-    name = "mercaury_data_engine_storage03"
+    name = "mercaury_backend_data002"
 
     storage_class = "REGIONAL"
 
@@ -36,18 +24,30 @@ resource "google_storage_bucket" "data_engine_storage03" {
 
   
 }
+resource "google_storage_bucket" "data_engine_storage003" {
+
+    name = "mercaury_data_engine_storage003"
+
+    storage_class = "REGIONAL"
+
+    project = "mytasks-362509"
+
+    location = "us-central1"
+
+  
+}
 
 
 
 
-resource "google_service_account" "mercaury_backend_enginneers001" {
+resource "google_service_account" "mercaury_backend_enginneers0001" {
   project = "mytasks-362509"
-  account_id   = "backendserviceaccount001"
+  account_id   = "backendserviceaccount0001"
   display_name = "A service account for backend that only mentioned users can use"
 }
 
 resource "google_service_account_iam_binding" "admin-account-iam" {
-  service_account_id = google_service_account.mercaury_backend_enginneers001.name
+  service_account_id = google_service_account.mercaury_backend_enginneers0001.name
 
   role               = "roles/iam.serviceAccountUser"
 
@@ -58,14 +58,14 @@ resource "google_service_account_iam_binding" "admin-account-iam" {
 }
 
 
-resource "google_service_account" "mercaury_frontend_enginneers002" {
+resource "google_service_account" "mercaury_frontend_enginneers0002" {
   project = "mytasks-362509"
-  account_id   = "frontendserviceaccount"
+  account_id   = "frontendserviceaccount0002"
   display_name = "A service account frontend that only mentioned users can use"
 }
 
 resource "google_service_account_iam_binding" "security-account-iam" {
-  service_account_id = google_service_account.mercaury_frontend_enginneers002.name
+  service_account_id = google_service_account.mercaury_frontend_enginneers0002.name
 
   role               = "roles/iam.securityAdmin"
 
@@ -76,16 +76,16 @@ resource "google_service_account_iam_binding" "security-account-iam" {
 }
 
 
-resource "google_service_account" "mercaury_data_engineers003" {
+resource "google_service_account" "mercaury_data_engineers0003" {
   project = "mytasks-362509"
-  account_id = "dataengineersserviceaccount"
+  account_id = "dataengineersserviceaccount0003"
   display_name = "A service account for data_engineers that only mentioned users can use"
   
 }
 
 resource "google_service_account_iam_binding" "service_account_deleter" {
 
-  service_account_id =google_service_account.mercaury_data_engineers003.name
+  service_account_id =google_service_account.mercaury_data_engineers0003.name
   role = "roles/iam.serviceAccountDeleter"
 
   members = [
